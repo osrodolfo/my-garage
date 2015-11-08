a50aaf4bf8ee5a9e48fac1a2396e126617633116
oscar <osrodolfo@mesoamericana.edu.gt>
2015-11-08 01:31:27 -0600

    Ampliando plantillas, agregando una plantilla para ver detalles

--- garage/urls.py
+++ garage/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.Listar),
+    url(r'^dato/(?P<pk>[0-9]+)/$', views.Detalles),
 ]
--- garage/views.py
+++ garage/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Registro
+from django.shortcuts import render, get_object_or_404
 
 def Listar(request):
     dato = Registro.objects.filter(fecha__lte=timezone.now()).order_by('fecha')
     return render(request, 'garage/Listar_carros.html', {'dato':dato})
+
+def Detalles(request, pk):
+        dato = get_object_or_404(Registro, pk=pk)
+        return render(request, 'garage/detalle_carro.html', {'dato': dato})
